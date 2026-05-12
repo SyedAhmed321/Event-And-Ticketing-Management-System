001f25ae177c0e9547664b0a97444200dccbce73
SyedAhmed321 <isyedahmed110@gmail.com>
2026-05-12 17:17:33 +0500

    mongodb localhost setup done and tested + nugetpackages installed

--- Event & Ticketing Management System/Configurations/MongoDbService.cs
+++ Event & Ticketing Management System/Configurations/MongoDbService.cs
@@ -1,6 +1,24 @@
-﻿namespace Event___Ticketing_Management_System.Configurations
+﻿using MongoDB.Driver;
+
+namespace Event___Ticketing_Management_System.Configurations
 {
     public class MongoDbService
     {
+        private readonly IMongoDatabase _database;
+
+        public MongoDbService(IConfiguration configuration)
+        {
+            var connectionString =
+                configuration["MongoDbSettings:ConnectionString"];
+
+            var databaseName =
+                configuration["MongoDbSettings:DatabaseName"];
+
+            var client = new MongoClient(connectionString);
+
+            _database = client.GetDatabase(databaseName);
+        }
+
+        public IMongoDatabase Database => _database;
     }
 }
